65757e2e9d1acdbd2e9bcc881c250d7cd3020412
Ben Visness <bvisness@mozilla.com>
2022-11-07 15:40:12 -0600

    Add field parsing

--- main.go
+++ main.go
@@ -13,49 +13,62 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var reFrame = regexp.MustCompile(`^frame\s*\d+\s*(\d+)\s*(\d+)$`)
-var reCount = regexp.MustCompile(`^count\s*(\d+)$`)
+var reWhitespace = regexp.MustCompile(`\s+`)
+var reCount = regexp.MustCompile(`^\d+$`)
 var reOffset = regexp.MustCompile(`\+[^+]*$`)
 var reCFunctionArgument = regexp.MustCompile(`(::.*)[(<].*`)
+
+type State int
+
+const (
+	StateExpectingNewFrame State = iota + 1 // waiting for fields or the first entry in a stack
+	StateInFrame                            // waiting for the count to end the frame
+)
 
 func main() {
 	rootCmd := &cobra.Command{
 		Use: "dtrace2spall",
 		Run: func(cmd *cobra.Command, args []string) {
 			var f io.Writer = os.Stdout
-			if out, err := cmd.PersistentFlags().GetString("out"); err == nil && out != "" {
+			if out, _ := cmd.PersistentFlags().GetString("out"); out != "" {
 				if out == "-" {
 					f = os.Stdout
 				} else {
+					var err error
 					f, err = os.Create(out)
 					if err != nil {
 						panic(err)
 					}
 				}
 
-				passthrough, err := cmd.PersistentFlags().GetBool("passthrough")
-				if err != nil {
-					panic(err)
-				}
+				passthrough, _ := cmd.PersistentFlags().GetBool("passthrough")
 				if passthrough && f == os.Stdout {
 					fmt.Fprintln(os.Stderr, "ERROR: --passthrough requires the use of --out (because --passthrough needs stdout)")
 					os.Exit(1)
 				}
 
-				freq, err := cmd.PersistentFlags().GetInt("freq")
-				if err != nil {
-					panic(err)
-				}
+				freq, _ := cmd.PersistentFlags().GetInt("freq")
+				fields, _ := cmd.PersistentFlags().GetStringSlice("fields")
 
 				p := spall.NewProfile(f, 1_000_000/spall.TimestampUnit(freq)) // (µs/s) / (samples/s) = µs/sample
 				defer p.Close()
 				e := p.NewEventer()
 				defer e.Close()
 
+				state := StateExpectingNewFrame
 				var pid, tid uint32
 				var currentStack []string // the previous entry's stack
 				var stackEntries []string // the stack entries we've built up so far (reverse order because hooray dtrace)
 				var now float64 = 0
+
+				addStackEntry := func(line string) {
+					line = reOffset.ReplaceAllString(line, "")
+					line = reCFunctionArgument.ReplaceAllString(line, "$1")
+					if line == "" {
+						line = "-"
+					}
+					stackEntries = append(stackEntries, line)
+				}
 
 				scanner := bufio.NewScanner(os.Stdin)
 				for scanner.Scan() {
@@ -66,29 +79,50 @@ func main() {
 
 					line = strings.TrimSpace(line)
 					if line == "" {
-						// norp
-					} else if m := reFrame.FindStringSubmatch(line); m != nil {
-						// Start a new frame
-						pidStr := m[1]
-						tidStr := m[2]
-						pidU64, err := strconv.ParseUint(pidStr, 10, 32)
-						if err != nil {
-							panic(err)
-						}
-						tidU64, err := strconv.ParseUint(tidStr, 10, 32)
-						if err != nil {
-							panic(err)
-						}
-						pid = uint32(pidU64)
-						tid = uint32(tidU64)
-						stackEntries = stackEntries[:0] // reset stack in place (reuse memory)
-					} else if m := reCount.FindStringSubmatch(line); m != nil {
-						// End of a stack; track the stuff
-						countStr := m[1]
+						// Nothin'. Must be between frames.
+						state = StateExpectingNewFrame
+					} else if state == StateExpectingNewFrame {
+						// Non-empty line starts a new frame
 
-						count, err := strconv.Atoi(countStr)
+						// Parse fields, or just start directly at the trace
+						if len(fields) == 0 {
+							addStackEntry(line)
+						} else {
+							fieldStrs := reWhitespace.Split(line, -1)
+							if len(fieldStrs) != len(fields) {
+								fmt.Fprintf(os.Stderr, "ERROR: Expected %d fields but got %d. Problematic line:\n", len(fields), len(fieldStrs))
+								fmt.Fprintln(os.Stderr, line)
+								os.Exit(1)
+							}
+
+							for i, fieldStr := range fieldStrs {
+								switch fields[i] {
+								case "pid":
+									pidU64, err := strconv.ParseUint(fieldStr, 10, 32)
+									if err != nil {
+										fmt.Fprintf(os.Stderr, "ERROR: \"%s\" is not a valid pid.\n", fieldStr)
+										os.Exit(1)
+									}
+									pid = uint32(pidU64)
+								case "tid":
+									tidU64, err := strconv.ParseUint(fieldStr, 10, 32)
+									if err != nil {
+										fmt.Fprintf(os.Stderr, "ERROR: \"%s\" is not a valid tid.\n", fieldStr)
+										os.Exit(1)
+									}
+									tid = uint32(tidU64)
+								default:
+									// Ignore all others.
+								}
+							}
+						}
+
+						state = StateInFrame
+					} else if state == StateInFrame && reCount.MatchString(line) {
+						// End of a stack; track the stuff
+						count, err := strconv.Atoi(line)
 						if err != nil {
-							panic(fmt.Errorf("'%s' is not a valid sample count", countStr))
+							panic(fmt.Errorf("'%s' is not a valid sample count", line))
 						}
 						now += float64(count)
 
@@ -107,14 +141,16 @@ func main() {
 								currentStack = append(currentStack, entry)
 							}
 						}
-					} else {
+
+						// Reset frame
+						pid, tid = 0, 0
+						stackEntries = stackEntries[:0] // reset stack in place (reuse memory)
+						state = StateExpectingNewFrame
+					} else if state == StateInFrame {
 						// One entry in a stack
-						line = reOffset.ReplaceAllString(line, "")
-						line = reCFunctionArgument.ReplaceAllString(line, "$1")
-						if line == "" {
-							line = "-"
-						}
-						stackEntries = append(stackEntries, line)
+						addStackEntry(line)
+					} else {
+						panic("bad state!")
 					}
 				}
 				if err := scanner.Err(); err != nil {
@@ -130,7 +166,7 @@ func main() {
 	}
 	rootCmd.PersistentFlags().IntP("freq", "f", 1000, "The frequency of profile sampling, in Hz.")
 	rootCmd.PersistentFlags().StringP("out", "o", "", "The file to write the results to. Use \"-\" for stdout.")
-	rootCmd.PersistentFlags().Bool("combine", false, "Combine identical stacks to make traces less noisy. (Doing so loses information about ordering.)")
+	rootCmd.PersistentFlags().StringSlice("fields", nil, "An array of fields preceding each stack. Valid fields: pid, tid. Any unrecognized fields will be ignored (consider using \"-\" for any such fields).")
 	rootCmd.PersistentFlags().Bool("passthrough", false, "Pass the input data through to stdout, making this tool invisible to pipelines. Requires --out.")
 
 	if err := rootCmd.Execute(); err != nil {
